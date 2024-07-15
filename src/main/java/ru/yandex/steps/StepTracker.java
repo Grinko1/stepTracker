3c745d6fdfd14eafac1dfa86ec918cf752e3d31c
Grinko1 <nadenka64013730@gmail.com>
2024-07-15 13:05:22 +0900

    updated print monthly statistics

--- src/main/java/ru/yandex/steps/StepTracker.java
+++ src/main/java/ru/yandex/steps/StepTracker.java
@@ -4,7 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class StepTracker {
+    // month: {day: steps}
     HashMap<Integer, HashMap<Integer, Integer>> table = new HashMap<>();
+
+    // day: steps
     HashMap<Integer, Integer> month = new HashMap<>();
     private int goal = 10000;
 
@@ -13,7 +16,7 @@ public class StepTracker {
             HashMap<Integer, Integer> month = new HashMap<>();
             int daysInMonth = getDaysInMonth(i);
             for (int j = 1; j <= daysInMonth; j++) {
-                // day: steps
+
                 month.put(j, 0);
             }
             table.put(i, month);
@@ -28,58 +31,78 @@ public class StepTracker {
         this.goal = goal;
     }
 
-    public void printFullData(){
-        for (Map.Entry<Integer, HashMap<Integer, Integer>> entry: table.entrySet()){
-            System.out.println("\n" +getNameOfMonth( entry.getKey()) +":\n");
-            for (Map.Entry<Integer, Integer> day : entry.getValue().entrySet()){
-                System.out.println(day.getKey()+"день: пройдено шагов "+ day.getValue());
+    public void printFullData() {
+        for (Map.Entry<Integer, HashMap<Integer, Integer>> entry : table.entrySet()) {
+            System.out.println("\n" + getNameOfMonth(entry.getKey()) + ":\n");
+            for (Map.Entry<Integer, Integer> day : entry.getValue().entrySet()) {
+                System.out.println(day.getKey() + "день: пройдено шагов " + day.getValue());
 
             }
 
         }
     }
-    public void updateValueInTable(int month, int day, int steps){
+
+    public void updateValueInTable(int month, int day, int steps) {
         table.get(month).put(day, steps);
     }
+
     private int getDaysInMonth(int month) {
         return switch (month) {
             case 1, 3, 5, 7, 8, 10, 12 -> 31; // Months with 31 days
             case 4, 6, 9, 11 -> 30; // Months with 30 days
             case 2 -> 28; // February with 28 days (assuming non-leap year)
-            default -> throw new IllegalStateException("Unexpected value: " + month);
+            default -> throw new IllegalStateException("Нет месяца : " + month);
         };
     }
-    private String getNameOfMonth(int n){
-        return switch(n){
-            case 1 ->"Январь";
-            case 2 ->"Февраль";
-            case 3 ->"Март";
-            case 4 ->"Апрель";
-            case 5 ->"Май";
-            case 6 ->"Июнь";
-            case 7 ->"Июль";
-            case 8 ->"Август";
-            case 9 ->"Сентябрь";
-            case 10 ->"Октябрь";
-            case 11 ->"Ноябрь";
-            case 12 ->"Декабрь";
-            default -> throw new IllegalStateException("Unexpected value: " + n);
+
+    private String getNameOfMonth(int n) {
+        return switch (n) {
+            case 1 -> "Январь";
+            case 2 -> "Февраль";
+            case 3 -> "Март";
+            case 4 -> "Апрель";
+            case 5 -> "Май";
+            case 6 -> "Июнь";
+            case 7 -> "Июль";
+            case 8 -> "Август";
+            case 9 -> "Сентябрь";
+            case 10 -> "Октябрь";
+            case 11 -> "Ноябрь";
+            case 12 -> "Декабрь";
+            default -> throw new IllegalStateException("Нет месяца : " + n);
         };
     }
 
     public void printMonthData(int month) {
         HashMap<Integer, Integer> monthData = table.get(month);
-
-        int totalSteps= 0;
+        StringBuilder res = new StringBuilder();
+        int totalSteps = 0;
         int achieveGoals = 0;
-        for (Map.Entry<Integer, Integer> entry : monthData.entrySet()){
-            totalSteps += entry.getValue();
-            if (entry.getValue() >= goal){
-                achieveGoals+=1;
-            }
+        int maxSteps =0;
 
-            System.out.println(entry.getKey()+" - "+ entry.getValue()+" шагов");
+        for (Map.Entry<Integer, Integer> entry : monthData.entrySet()) {
+
+            int steps = entry.getValue();
+            totalSteps += steps;
+            if (steps >= goal) {
+                achieveGoals += 1;
+
+            }
+            if (steps > maxSteps){
+                maxSteps = steps;
+            }
+            res.append(entry.getKey()).append(" день: ").append(entry.getValue()).append(" шагов").append(", ");
         }
-        System.out.println("Всего шагов "+ totalSteps +" | Цель достигнута "+achieveGoals+" раз(a)");
+        int totalKkl = (totalSteps * 50)/1000;
+        System.out.println("Статистика за "+ getNameOfMonth(month));
+        System.out.println(res.substring(0, res.length()-1));
+        System.out.println(
+                "Всего шагов " + totalSteps
+                 + " | максимально пройдено "+ maxSteps
+                 + " | Среднее к-во шагов " + (totalSteps/30)
+                +" | Пройдено км "+ (totalSteps * 0.75)
+                + " | К-во сожженных калорий "+ totalKkl
+                + " | Лучшая серия: "+  (0) + " дней"
+                + " | Цель достигнута " + achieveGoals + " раз(a)");
     }
 }
