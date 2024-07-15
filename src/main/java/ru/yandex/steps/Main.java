dcefebd79a7e23515829212a6ca83714406cc97d
Grinko1 <nadenka64013730@gmail.com>
2024-07-15 12:17:31 +0900

    main logic done

--- src/main/java/ru/yandex/steps/Main.java
+++ src/main/java/ru/yandex/steps/Main.java
@@ -1,7 +1,50 @@
 package ru.yandex.steps;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        StepTracker stepTracker  = new StepTracker();
+        Scanner scanner = new Scanner(System.in);
+        printMenu();
+        int action = scanner.nextInt();
 
+        while (action != 0) {
+            switch (action) {
+                case 1 -> {
+                    System.out.println("Введите цель по шагам: ");
+                    int goal = scanner.nextInt();
+                    stepTracker.setGoal(goal);
+                    System.out.println("Новая цель по к-ву шагов " + goal);
+                }
+                case 2 -> {
+                    System.out.println("Введите дату и к-во шагов (формата \"ДД.ММ ШАГИ\"): ");
+                    scanner.nextLine();  // Consume newline
+                    String input = scanner.nextLine();
+                    String[] parts = input.split(" ");
+                    String[] dateParts = parts[0].split("\\.");
+                    int day = Integer.parseInt(dateParts[0]);
+                    int month = Integer.parseInt(dateParts[1]);
+                    int steps = Math.abs(Integer.parseInt(parts[1]));
+                    stepTracker.updateValueInTable(month, day, steps);
+                    System.out.println("Значение обновлено " +0+ day + "/" + 0+month +" - "+ steps+" шагов");
+                }
+                case 3 -> {
+                    System.out.println("Введите месяц (от 1 до 12): ");
+                    int month = scanner.nextInt();
+                    stepTracker.printMonthData(month);
+                }
+                case 4 -> stepTracker.printFullData();
+                default -> System.out.println("Неверное значение. Попробуйте ввести другое значение.");
+            }
+            printMenu();
+            action = scanner.nextInt();
+        }
+        scanner.close();
+        System.out.println("Завершение работы");
+    }
+    public  static void printMenu(){
+        System.out.println("\nВведи цифру действия");
+        System.out.println("1: ввести цель по шагам | 2: ввести пройденные шаги | 3: получить статистику за месяц | 4: получить статистику за год | 0: завершить");
     }
 }
--- src/main/java/ru/yandex/steps/StepTracker.java
+++ src/main/java/ru/yandex/steps/StepTracker.java
@@ -1,4 +1,85 @@
 package ru.yandex.steps;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StepTracker {
+    HashMap<Integer, HashMap<Integer, Integer>> table = new HashMap<>();
+    HashMap<Integer, Integer> month = new HashMap<>();
+    private int goal = 10000;
+
+    public StepTracker() {
+        for (int i = 1; i <= 12; i++) {
+            HashMap<Integer, Integer> month = new HashMap<>();
+            int daysInMonth = getDaysInMonth(i);
+            for (int j = 1; j <= daysInMonth; j++) {
+                // day: steps
+                month.put(j, 0);
+            }
+            table.put(i, month);
+        }
+    }
+
+    public int getGoal() {
+        return goal;
+    }
+
+    public void setGoal(int goal) {
+        this.goal = goal;
+    }
+
+    public void printFullData(){
+        for (Map.Entry<Integer, HashMap<Integer, Integer>> entry: table.entrySet()){
+            System.out.println("\n" +getNameOfMonth( entry.getKey()) +":\n");
+            for (Map.Entry<Integer, Integer> day : entry.getValue().entrySet()){
+                System.out.println(day.getKey()+"день: пройдено шагов "+ day.getValue());
+
+            }
+
+        }
+    }
+    public void updateValueInTable(int month, int day, int steps){
+        table.get(month).put(day, steps);
+    }
+    private int getDaysInMonth(int month) {
+        return switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> 31; // Months with 31 days
+            case 4, 6, 9, 11 -> 30; // Months with 30 days
+            case 2 -> 28; // February with 28 days (assuming non-leap year)
+            default -> throw new IllegalStateException("Unexpected value: " + month);
+        };
+    }
+    private String getNameOfMonth(int n){
+        return switch(n){
+            case 1 ->"Январь";
+            case 2 ->"Февраль";
+            case 3 ->"Март";
+            case 4 ->"Апрель";
+            case 5 ->"Май";
+            case 6 ->"Июнь";
+            case 7 ->"Июль";
+            case 8 ->"Август";
+            case 9 ->"Сентябрь";
+            case 10 ->"Октябрь";
+            case 11 ->"Ноябрь";
+            case 12 ->"Декабрь";
+            default -> throw new IllegalStateException("Unexpected value: " + n);
+        };
+    }
+
+    public void printMonthData(int month) {
+        HashMap<Integer, Integer> monthData = table.get(month);
+
+        int totalSteps= 0;
+        int achieveGoals = 0;
+        for (Map.Entry<Integer, Integer> entry : monthData.entrySet()){
+            totalSteps += entry.getValue();
+            if (entry.getValue() >= goal){
+                achieveGoals+=1;
+            }
+
+            System.out.println(entry.getKey()+" - "+ entry.getValue()+" шагов");
+        }
+        System.out.println("Всего шагов "+ totalSteps +" | Цель достигнута "+achieveGoals+" раз(a)");
+    }
 }
